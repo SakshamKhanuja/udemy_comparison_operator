e5b2f92b50ec001c54e894952b834e655b9e9c8b
SakshamKhanuja <94056845+SakshamKhanuja@users.noreply.github.com>
2022-07-28 22:42:45 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // 1. Equal to operator - "==".
+        int topScore = 100;
+
+        // Checks whether "topScore" equal to 100.
+        if (topScore == 100) {
+            System.out.println("You got the high score.");
+        }
+
+        // 2. Not equal to operator - "!=".
+
+        // Checks whether "topScore" is not equal 100.
+        if (topScore != 100) {
+            System.out.println("You haven't got the high score.");
+        }
+
+        // 3. Greater than operator - ">".
+
+        // Checks whether "topScore" is greater than 100.
+        if (topScore > 100) {
+            System.out.println("You got more than 100!");
+        }
+
+        // 4. Greater than or equal to operator - ">=".
+
+        // Checks whether "topScore" is greater than or equal to 100.
+        if (topScore >= 100) {
+            System.out.println("You scored 100 or more!");
+        }
+
+        // 5. Less than operator - "<".
+
+        // Checks whether "topScore" is less than 100.
+        if (topScore < 100) {
+            System.out.println("You scored less than 100.");
+        }
+
+        // 6. Less than or eqaul to operator - "<=".
+
+        // Checks whether "topScore" is less than or equal to 100.
+        if (topScore <= 100) {
+            System.out.println("You scored 100 or less.");
+        }
+    }
+}
